dad53788daa91bb4f1c0beb65c8790e9055ecea5
Rmesonez <rmesonez@gmail.com>
2023-04-11 17:05:29 -0600

    first5

--- src/store/slices/cart.slice.js
+++ src/store/slices/cart.slice.js
@@ -9,6 +9,12 @@ export const cartSlice = createSlice({
         setCartItems: (state, action) => {
             return action.payload;
         },
+        addToCart: (state, action) => {
+            return [...state, action.payload];
+        },
+        deleteProductCart: (state, action) => {
+            return state.filter((item) => item.id !== action.payload);
+        },
     }
 });
 
@@ -23,7 +29,27 @@ export const getCartItemsThunk = () => (dispatch) => {
     })
 }
 
+export const addToCartThunk = (product) => (dispatch) => {
+    axios.post('https://e-commerce-api-v2.academlo.tech/api/v1/cart', product, getConfig())
+    .then((res) => {
+        console.log(res?.data)
+        // dispatch(addToCart(res?.data))
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+}
 
+export const deleteProductCartThunk = (id) => (dispatch) => {
+    axios.delete(`https://e-commerce-api-v2.academlo.tech/api/v1/cart/${id}`, getConfig())
+    .then((res) => {
+        console.log(res?.data)
+        // dispatch(deleteProductCart(id))
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+}
 
 
 export const { setCartItems } = cartSlice.actions;
